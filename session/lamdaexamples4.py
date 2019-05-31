055cb16e202730c2efa121c7419d3a3dc1343eb4
Pawan <pawansonu4@gmail.com>
2019-05-31 07:53:46 +0530

    Training files added

---
+++ session/lamdaexamples4.py
@@ -0,0 +1,7 @@
+# Python code to illustrate
+# reduce() with lambda()
+# to get sum of a list
+from functools import reduce
+li = [5, 8, 10, 20, 50, 100]
+sum1 = reduce((lambda x, y: x + y), li)
+print(sum1)
